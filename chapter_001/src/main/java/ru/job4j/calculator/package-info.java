1e92f08513cbe9dbf9bfa445d9be07eab50f1287
Alexey Miroshnichenko <lxmir@ya.ru>
2017-08-10 00:19:42 +0700

    исправления ошибок, которые показала IDEA

---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for Calculator task.
+*
+* @author Aleksey Miroshnichenko (mailto:lxmir@ya.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculator;
---
+++ chapter_001/src/test/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+* //Package for test Calculator.
+*
+* @author Aleksey Miroshnichenko (mailto:lxmir@ya.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculator;
